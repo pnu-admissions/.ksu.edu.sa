5bd2c7a409b552f04b3acd977c2b832cdaba021a
pnu-admissions <abdullahyourdomain@gmail.com>
2025-11-15 05:08:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-<!-- رابط CDN لخطوط Font Awesome -->
-<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
-
 <!-- =================== بيانات الطالب =================== -->
 <script>
 const studentData = {
